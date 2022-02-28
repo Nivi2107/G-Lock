074a97556fd698ddc7f7943a8972c3c30bc44baa
Nivetha A <91119703+Nivi2107@users.noreply.github.com>
2022-02-28 11:02:48 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 14 19:58:07 2021
+
+@author: Ram
+"""
+
+# import the Flask class from the flask module
+from flask import Flask, render_template, request, jsonify
+import emoDetectv1
+
+# create the application object
+app = Flask(__name__,static_url_path="",static_folder="static")
+
+# use decorators to link the function to a url
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/emotion', methods = ['POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['image']
+      f.save("faceimage.jpg")
+      return jsonify(emotion=emoDetectv1.emotion())
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
